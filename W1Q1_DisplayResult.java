fa3b52335596a352b82f36ca078f48907de08b5b
sanjana270307-cpu <sanjana270307@gmail.com>
2026-04-14 18:24:03 +0530

    Added Week 1 - Q1 Display Result

---
+++ W1Q1_DisplayResult.java
@@ -0,0 +1,18 @@
+// Program to display student result using variables and proper formatting
+
+class W1Q1_DisplayResult {
+    public static void main(String[] args) {
+
+        // Creating variables
+        String name = "Sam";
+        int rollNumber = 1;
+        double percentMarks = 99.99;
+        char result = 'P';
+
+        // Displaying output
+        System.out.println("Displaying Result:\n" + name +
+                           " with Roll Number " + rollNumber +
+                           " has scored " + percentMarks +
+                           "% Marks and Result is " + result);
+    }
+}
